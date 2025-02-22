a1e9081b16873e45d7abd0828fb85945a6f94622
iqra <im4356927@gmail.com>
2025-02-22 13:14:13 -0800

    removal of extra import

--- routes/chat.route.js
+++ routes/chat.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { auth } from "../middlerware/auth.middleware.js";
-import {getmembersfromchatId, addmember, chatFriend, createMessage, getChats, getFriends, getGroups, getMessages, leaveGroup, newGroup, notMyFriend, removeMember, sendAttachments, newChat, deleteChat, ChatBot, getChatInfo } from "../controller/chat.controller.js";
+import {getmembersfromchatId, addmember, chatFriend, createMessage, getChats, getFriends, getGroups, getMessages, leaveGroup, newGroup, notMyFriend, removeMember, newChat, deleteChat,  getChatInfo } from "../controller/chat.controller.js";
 import { newGroupValidator } from "../utilities/validator.js";
 import { upload } from "../middlerware/multer.js";
 import { chat } from "../schema/chat.schema.js";
@@ -14,7 +14,6 @@ chatRouter.route('/get-group').get(auth,getGroups)
 chatRouter.route('/addmember').post(auth,addmember)
 chatRouter.route('/left/:id').post(leaveGroup)
 chatRouter.route('/getmsg/:id').get(getMessages)  // get messages
-// chatRouter.route('/sendAttachment').post(auth,upload.array("attachment",5),sendAttachments);
 chatRouter.route('/getFriends').get(auth,getFriends);
 chatRouter.route('/friendchat/:id').get(auth,chatFriend);
 chatRouter.route('/getNotMyFriend').get(auth,notMyFriend);
